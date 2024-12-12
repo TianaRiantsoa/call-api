0ac63d32ef2c5687da58ffe27cf98bc688ada07f
Tiana Riantsoa RAHARISON <traharison@vaisonet.com>
2024-12-12 16:32:21 +0300

    MAJ

--- views/shopify/productresults.php
+++ views/shopify/productresults.php
@@ -4,7 +4,7 @@ use Shopify\Auth\FileSessionStorage;
 use Shopify\Clients\Graphql;
 use Shopify\Context;
 use yii\helpers\Html;
-
+use SPF\init;
 /** @var yii\web\View $this */
 /** @var app\models\Shopify $model */
 
@@ -20,14 +20,15 @@ $pwd = Html::encode($model->password);
 $sct = Html::encode($model->secret_key);
 $ref = Html::encode($ref);
 
-$scopes = 'read_analytics, read_assigned_fulfillment_orders, read_customer_events, read_customers, read_discounts, read_discovery, read_draft_orders, read_files, read_fulfillments, read_gdpr_data_request, read_gift_cards, read_inventory, read_legal_policies, read_locations, read_marketing_events, read_merchant_managed_fulfillment_orders, read_online_store_navigation, read_online_store_pages, read_order_edits, read_orders, read_packing_slip_templates, read_payment_customizations, read_payment_terms, read_pixels, read_price_rules, read_product_feeds, read_product_listings, read_products, read_publications, read_purchase_options, read_reports, read_resource_feedbacks, read_returns, read_channels, read_script_tags, read_shipping, read_locales, read_markets, read_shopify_payments_accounts, read_shopify_payments_bank_accounts, read_shopify_payments_disputes, read_shopify_payments_payouts, read_content, read_themes, read_third_party_fulfillment_orders, read_translations, read_all_cart_transforms, read_cart_transforms, read_custom_fulfillment_services, read_delivery_customizations, read_fulfillment_constraint_rules, read_gates';
+// $scopes = 'read_analytics, read_assigned_fulfillment_orders, read_customer_events, read_customers, read_discounts, read_discovery, read_draft_orders, read_files, read_fulfillments, read_gdpr_data_request, read_gift_cards, read_inventory, read_legal_policies, read_locations, read_marketing_events, read_merchant_managed_fulfillment_orders, read_online_store_navigation, read_online_store_pages, read_order_edits, read_orders, read_packing_slip_templates, read_payment_customizations, read_payment_terms, read_pixels, read_price_rules, read_product_feeds, read_product_listings, read_products, read_publications, read_purchase_options, read_reports, read_resource_feedbacks, read_returns, read_channels, read_script_tags, read_shipping, read_locales, read_markets, read_shopify_payments_accounts, read_shopify_payments_bank_accounts, read_shopify_payments_disputes, read_shopify_payments_payouts, read_content, read_themes, read_third_party_fulfillment_orders, read_translations, read_all_cart_transforms, read_cart_transforms, read_custom_fulfillment_services, read_delivery_customizations, read_fulfillment_constraint_rules, read_gates';
 
-// $apiVersion = ApiVersion::LATEST;
+// // $apiVersion = ApiVersion::LATEST;
 
-Context::initialize($api, $sct, Html::encode($scopes), $url, new FileSessionStorage('/tmp/php_sessions'));
+// Context::initialize($api, $sct, Html::encode($scopes), $url, new FileSessionStorage('/tmp/php_sessions'));
 
-$client = new Graphql($url, $pwd);
+// $client = new Graphql($url, $pwd);
 
+$init = init::InitShopify($url,$api, $pwd, $sct);
 
 $query = <<<QUERY
     query {
@@ -55,7 +56,7 @@ $query = <<<QUERY
 
     
 
-$response = $client->query(["query" => $query]);
+$response = $init->query(["query" => $query]);
 
 $contents = $response->getBody()->getContents();
 
