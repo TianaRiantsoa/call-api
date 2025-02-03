8eca41833231353b485ab0cdeb83e6be4949a78b
Tiana Riantsoa RAHARISON <traharison@vaisonet.com>
2025-02-03 16:05:36 +0100

    MAJ SQLite

--- config/db.php
+++ config/db.php
@@ -2,12 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ws',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'sqlite:' . __DIR__ . '/../db/ws.sqlite', // Chemin vers ton fichier SQLite
     'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
+    // Si nécessaire, tu peux activer le cache de schéma pour améliorer la performance
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
