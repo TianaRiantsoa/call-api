bf7389685d151b471f0f1b40328c2cea9f3c6d3a
Tiana Riantsoa RAHARISON <traharison@vaisonet.com>
2025-02-03 11:46:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,4 @@
 // Redirection vers le dossier 'web' pour accéder à 'index.php'
 header('Location: web/index.php');
 exit(); // Assurez-vous d'utiliser exit() après une redirection pour arrêter l'exécution du script.
-
-
 ?>
