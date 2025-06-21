e805d8462887ee90ad16758ca44aebffcc0818f4
Tiana Riantsoa RAHARISON <traharison@vaisonet.com>
2025-06-21 11:16:01 +0200

    test db sqlite/mysql

--- config/db.php
+++ config/db.php
@@ -10,3 +10,17 @@ return [
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
 ];
+
+
+// return [
+//     'class' => 'yii\db\Connection',
+//     'dsn' => 'mysql:host=brjozkbwdmpn8ypzkh3b-mysql.services.clever-cloud.com;dbname=brjozkbwdmpn8ypzkh3b',
+//     'username' => 'uqsvll7kfttc3d7z',
+//     'password' => '6I1TDiKvJZQspDosvUOH',
+//     'charset' => 'utf8mb4',
+
+//     // Si nécessaire, tu peux activer le cache de schéma pour améliorer la performance
+//     //'enableSchemaCache' => true,
+//     //'schemaCacheDuration' => 60,
+//     //'schemaCache' => 'cache',
+// ];
