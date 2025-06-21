7fee326416c911d63c60e24540dbe3cadd6d76a0
Tiana Riantsoa RAHARISON <traharison@vaisonet.com>
2025-06-21 11:51:19 +0200

    MAJ DB connection

--- config/db.php
+++ config/db.php
@@ -1,16 +1,25 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'sqlite:' . __DIR__ . '/../db/ws.sqlite', // Chemin vers ton fichier SQLite
-    'charset' => 'utf8',
+// Configuration de la connexion à la base de données SQLite pour l'application Yii2
+// Assure-toi que le fichier SQLite existe à l'emplacement spécifié
+// Si tu souhaites utiliser une base de données SQLite, tu peux utiliser le code suivant :
+// Base de données SQLite pour l'application Yii2
 
-    // Si nécessaire, tu peux activer le cache de schéma pour améliorer la performance
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
+// return [
+//     'class' => 'yii\db\Connection',
+//     'dsn' => 'sqlite:' . __DIR__ . '/../db/ws.sqlite', // Chemin vers ton fichier SQLite
+//     'charset' => 'utf8',
 
+//     // Si nécessaire, tu peux activer le cache de schéma pour améliorer la performance
+//     //'enableSchemaCache' => true,
+//     //'schemaCacheDuration' => 60,
+//     //'schemaCache' => 'cache',
+// ];
+
+
+// Si tu souhaites utiliser une base de données MySQL, tu peux décommenter et modifier la section suivante :$
+
+//Base de données MySQL Clever Cloud https://console.clever-cloud.com
 
 // return [
 //     'class' => 'yii\db\Connection',
@@ -24,3 +33,20 @@ return [
 //     //'schemaCacheDuration' => 60,
 //     //'schemaCache' => 'cache',
 // ];
+
+// Si tu souhaites utiliser une base de données MySQL, tu peux décommenter et modifier la section suivante :$
+
+//Base de données MySQL Free SQL DataBase https://www.freesqldatabase.com
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=sql7.freesqldatabase.com;dbname=sql7786037',
+    'username' => 'sql7786037',
+    'password' => 'yKiXXJtIK1',
+    'charset' => 'utf8mb4',
+
+    // Si nécessaire, tu peux activer le cache de schéma pour améliorer la performance
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
